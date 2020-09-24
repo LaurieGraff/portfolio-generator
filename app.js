028afd57aee18dfc8c5cda7767ab8865453de4a7
Laurie Graff <lauriegraff@Lauries-MacBook-Pro.local>
2020-09-24 16:47:47 -0600

    added html

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+//const printProfileData = (profileDataArr) => {
     //for (let i = 0; i < profileDataArr.lenth; i++){
        // console.log(profileDataArr[i])
     //}  
 
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
+//profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
+//};
 
-printProfileData(profileDataArgs);
+//printProfileData(profileDataArgs);
