b6f75103a1499bb5748998fbea8d3ddf9b4bac17
Laurie Graff <lauriegraff@Lauries-MacBook-Pro.local>
2020-09-25 10:12:45 -0600

    added inquirer module

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+    //if (err) throw new Error (err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-//const printProfileData = (profileDataArr) => {
-    //for (let i = 0; i < profileDataArr.lenth; i++){
-       // console.log(profileDataArr[i])
-    //}  
+    //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
 
 
-//profileDataArr.forEach(profileItem => console.log(profileItem));
-
-//};
-
-//printProfileData(profileDataArgs);
